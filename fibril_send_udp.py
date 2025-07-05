89bb55f13ef197635cde3f9211869f25173b6ecb
Novalynn <novalynn.or.nava@gmail.com>
2025-07-05 19:37:23 +0000

    minor change

--- fibril_send_udp.py
+++ fibril_send_udp.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     print("Sending OSC messages to 127.0.0.1:1762 every 100ms...")
     try:
         while True:
-            client.send_message("/test", 0)
+            client.send_message('/test', 0)
             print("Sent: /test 0")
             time.sleep(0.1)
     except KeyboardInterrupt:
